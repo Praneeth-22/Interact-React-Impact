ff8e88a2502ed61a9883bc036242da221d00874d
lokesh746 <vetchalokesh420@gmail.com>
2023-02-28 15:27:59 -0500

    small chat padding change

--- src/Components/Chat/Chatpage.js
+++ src/Components/Chat/Chatpage.js
@@ -6,8 +6,11 @@ import Chatdata from "./Chat";
 function Chat() {
   return (
     <div className="app-body">
-      <h1> Students Interaction</h1>
-      <div className="ui-body">
+      {/* <h1> Students Interaction</h1> */}
+      <div className="ui-body" style={{
+        margin: "10px 0px",
+        borderRadius: "10px",
+      }}>
         <Sidebar />
         <Chatdata />
       </div>
