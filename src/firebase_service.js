6c7f8759efca09c307abae9c9f2eee99a01609ed
Likhith Reddy Addela <likhithreddyaddela@dyn-169-226-91-12.wireless.albany.edu>
2023-03-02 15:58:50 -0500

    tokens updated

--- src/firebase_service.js
+++ src/firebase_service.js
@@ -2,13 +2,24 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDnYVLIQfDx1xF2QMwcbyKhe3i43RVrHjM",
-  authDomain: "se-team-o.firebaseapp.com",
-  projectId: "se-team-o",
-  storageBucket: "se-team-o.appspot.com",
-  messagingSenderId: "516312440546",
-  appId: "1:516312440546:web:f26aefe7dca2cc36bb88bb",
-  measurementId: "G-V0NLJWLS16",
+   //likhiths firebase tokens
+        apiKey: "AIzaSyAlYVMq9NeR-I8pSQvzcdjvNN2PCDQLjVQ",
+        authDomain: "authse-bddec.firebaseapp.com",
+        projectId: "authse-bddec",
+        storageBucket: "authse-bddec.appspot.com",
+        messagingSenderId: "513093921948",
+        appId: "1:513093921948:web:13c28519bb12fbcda31159"
+     
+  //praneeth-token    
+//   apiKey: "AIzaSyDnYVLIQfDx1xF2QMwcbyKhe3i43RVrHjM",
+//   authDomain: "se-team-o.firebaseapp.com",
+//   projectId: "se-team-o",
+//   storageBucket: "se-team-o.appspot.com",
+//   messagingSenderId: "516312440546",
+//   appId: "1:516312440546:web:f26aefe7dca2cc36bb88bb",
+//   measurementId: "G-V0NLJWLS16",
+
+//ajay tokens
 };
   
 
