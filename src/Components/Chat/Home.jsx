ed1ca7abcd521b2d922faee6f2aadb1e39623742
Ajay kumar Boreddy <ajaykumarboreddy@dyn-169-226-246-245.wireless.albany.edu>
2023-03-07 14:12:54 -0500

    chat components added

---
+++ src/Components/Chat/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Sidebar from './Sidebar'
+import Chat from './Chat'
+import '../Chat/style.scss';
+
+const Home = () => {
+  return (
+    <div className="home"> 
+    <div className="container">
+        <Sidebar/>
+        <Chat />
+
+    </div>
+      
+    </div>
+  )
+}
+
+export default Home
