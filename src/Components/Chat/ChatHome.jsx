68a81a1e5f529ec653d2d0e7dbb9cac4797e5fe8
lokesh746 <vetchalokesh420@gmail.com>
2023-03-23 19:51:47 -0400

    lokesh

--- src/Components/Chat/ChatHome.jsx
+++ src/Components/Chat/ChatHome.jsx
@@ -7,8 +7,8 @@ const Home = () => {
   return (
     <div className="home"> 
     <div className="container">
-        <Sidebar/>
-        <Chat />
+        {/* <Sidebar/> */}
+        {/* <Chat /> */}
 
     </div>
       
