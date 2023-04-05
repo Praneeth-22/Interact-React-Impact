40b3f7b014784d0900da5ae9c1b1a39ccd0327fb
Praneeth-22 <yennampraneeth@gmail.com>
2023-04-04 21:09:28 -0400

    added toast to profile change

--- src/Components/LoginFeature/ForgotPass.js
+++ src/Components/LoginFeature/ForgotPass.js
@@ -10,6 +10,7 @@ import { useUserAuth } from "../../context/UserContextApi";
 import { useState } from "react";
 import { Alert, stepClasses } from "@mui/material";
 import GoogleButton from "react-google-button";
+  import { toast, ToastContainer } from "react-toastify";
 function ForgotPass() {
     let navigate = useNavigate();
     const [email, setEmail] = useState("");
